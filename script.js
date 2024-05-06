c686c367e43d098ad9b243bbd53a5518844bd29f
Kainat zia <154695099+Kainat-zia@users.noreply.github.com>
2024-05-06 16:33:50 +0500

    javascript file

---
+++ script.js
@@ -0,0 +1,89 @@
+// alert("thank you")
+// document.write("leaern javascript")
+
+
+var first_name = "kainat"
+console.log(first_name);
+
+var name = "kainat";
+var age = 21;
+console.log("  my name is  "    +    name     +     "  my age is  " +     age);
+
+var message =  "my name is kainat";
+alert(message)
+
+var hellow
+console.log(hellow);
+
+var name = "kainat";
+var lastname = "zia"
+console.log(lastname);
+
+// let firstname = "laiba";
+// let firstname = 
+
+// let x = 12;
+// let y = 16;
+// let result = x * y;
+// console.log("My result is = "  +  result);
+
+let x = 12;
+let y = 16;
+// let result = x * y;
+console.log("My addition is  "  + (x + y));
+console.log("My subtraction is = "  + (x - y));
+console.log("My division is = "  + (x / y));
+console.log("My modulus is = "  + (x % y));
+console.log("My multiplication is = "  + (x * y));
+
+
+ var originalNum = 23;
+ var numToBeAdded = 7;
+ var newNum = originalNum + numToBeAdded;
+ console.log(originalNum + numToBeAdded );
+
+//legal words
+
+let firstName;
+let lastName;
+let lastname;
+let _dateOfBirth;
+let $email;
+let Age;
+let age21;
+let &kainat;
+let kainat!;
+let /question
+
+// illegal
+let 12;
+let 13return;
+let !return;
+let @23kainat;
+let "kainat";
+let ?kainat;
+
+let rose = "Flower"
+console.log(Rose);
+
+// task
+
+let var1 = "one"; var2 = "two"; var3 = "three";
+console.log("one"+ var1 + "two" + var2 + "var3"  + var3);
+
+let LastName;
+let secondName;
+let lastname;
+let _dateofBirth;
+let $Email;
+
+let 13;
+let 14return;
+let !Turn;
+let @24kainat;
+let "kainatzia";
+
+let costofticket = 600
+let buyingticket = 5
+let result =  costofticket *  buyingticket ;
+console.log( "total cost of ticket" +  result);
